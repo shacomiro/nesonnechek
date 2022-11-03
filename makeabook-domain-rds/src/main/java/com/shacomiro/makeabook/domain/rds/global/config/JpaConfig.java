b78fbd35b3cdcd205693eec5c212686b0f340520
Sunghyun Lee <98dltjdgus@naver.com>
2022-11-04 02:32:53 +0900

    refactor: JpaConfig 패키지 경로 수정
    - JpaConfig.java: JpaConfig 클래스를 global/config 패키지로 이동. 상위 경로에 있는 JpaRepository와 Entity를 스캔하기 위해 패키지 경로 기입.

--- makeabook-domain-rds/src/main/java/com/shacomiro/makeabook/domain/rds/global/config/JpaConfig.java
+++ makeabook-domain-rds/src/main/java/com/shacomiro/makeabook/domain/rds/global/config/JpaConfig.java
@@ -1,11 +1,11 @@
-package com.shacomiro.makeabook.domain.rds;
+package com.shacomiro.makeabook.domain.rds.global.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EntityScan
-@EnableJpaRepositories
+@EntityScan({"com.shacomiro.makeabook.domain.rds"})
+@EnableJpaRepositories({"com.shacomiro.makeabook.domain.rds"})
 public class JpaConfig {
 }
