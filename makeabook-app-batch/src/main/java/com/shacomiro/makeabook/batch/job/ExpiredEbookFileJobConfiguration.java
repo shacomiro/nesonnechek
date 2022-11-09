fb7a0564a0be055cdc189e481c27065dc4f70d9c
Sunghyun Lee <98dltjdgus@naver.com>
2022-11-09 23:20:37 +0900

    feat: 전자책 파일 만료 시 삭제하는 배치 작업 구현
    - ExpiredEbookFileJobConfiguration.java: 전자책 파일 목록을 불러온 후, 만료된 경우 서버 내부에 저장된 파일을 제거한 후 제거된 전자책 파일 정보를 반환하는 배치 작업 구현.

--- makeabook-app-batch/src/main/java/com/shacomiro/makeabook/batch/job/ExpiredEbookFileJobConfiguration.java
+++ makeabook-app-batch/src/main/java/com/shacomiro/makeabook/batch/job/ExpiredEbookFileJobConfiguration.java
@@ -1,13 +1,28 @@
 package com.shacomiro.makeabook.batch.job;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.shacomiro.makeabook.domain.rds.ebookfile.entity.EbookFile;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,20 +32,60 @@ import lombok.RequiredArgsConstructor;
 public class ExpiredEbookFileJobConfiguration {
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
+	private final EntityManagerFactory emf;
+	private static final int CHUNK_SIZE = 10;
 
 	@Bean
-	public Job job() {
-		return jobBuilderFactory.get("job")
-				.start(step())
+	public Job expiredEbookFileDeleteJob() {
+		return jobBuilderFactory.get("expiredEbookFileDeleteJob")
+				.start(expiredEbookFileDeleteStep())
 				.build();
 	}
 
 	@Bean
-	public Step step() {
-		return stepBuilderFactory.get("step")
-				.tasklet(((contribution, chunkContext) -> {
-					return RepeatStatus.FINISHED;
-				}))
+	@JobScope
+	public Step expiredEbookFileDeleteStep() {
+		return stepBuilderFactory.get("expiredEbookFileDeleteStep")
+				.<EbookFile, EbookFile>chunk(CHUNK_SIZE)
+				.reader(ebookFileReader())
+				.processor(deleteExpiredEbookFileProcessor())
+				.writer(ebookFilewriter())
+				.build();
+	}
+
+	@Bean
+	@StepScope
+	public JpaPagingItemReader<EbookFile> ebookFileReader() {
+		return new JpaPagingItemReaderBuilder<EbookFile>()
+				.name("expiredEbookFileReader")
+				.entityManagerFactory(emf)
+				.pageSize(CHUNK_SIZE)
+				.queryString("SELECT ef FROM EbookFile ef")
+				.build();
+	}
+
+	@Bean
+	@StepScope
+	public ItemProcessor<EbookFile, EbookFile> deleteExpiredEbookFileProcessor() {
+		return ebookFile -> {
+
+			if (ebookFile.isExpired()) {
+				Path targetPath = Paths.get("./files/ebook/", ebookFile.getFileType() + "/",
+						ebookFile.getUuid() + "." + ebookFile.getFileExtension()).normalize().toAbsolutePath();
+				boolean deleteResult = Files.deleteIfExists(targetPath);
+
+				return ebookFile;
+			} else {
+				return null;
+			}
+		};
+	}
+
+	@Bean
+	@StepScope
+	public JpaItemWriter<EbookFile> ebookFilewriter() {
+		return new JpaItemWriterBuilder<EbookFile>()
+				.entityManagerFactory(emf)
 				.build();
 	}
 }
