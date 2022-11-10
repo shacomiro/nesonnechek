a7f1390ee8f6298cb22ac0a22aa563d16a60780f
Sunghyun Lee <98dltjdgus@naver.com>
2022-11-11 00:35:07 +0900

    refactor: 업로드 파일을 임시 파일로 저장 후 처리하도록 로직 변경
    - EbookApi.java: 업로드 파일의 MultipartFile 정보를 매개변수로 그대로 넘기도록 변경.
    - EbookFileService.java: MultipartFile을 임시 파일로 저장한 후, 저장된 임시 파일들의 경로 정보를 갖는 객체를 ebookManager로 전달하도록 로직 수정.
    - ContentTempFileInfo.java: 임시 컨텐츠 파일들의 경로 정보를 갖는 도메인 클래스 정의.
    - EbookManager.java: 임시 파일의 텍스트 내용을 읽은 후 번역기로 전달하도록 로직 수정.
    - Epub2Translator.java: 별도의 바이트 처리 대신 문자열 리스트를 매개 변수로 넘겨받은 후 즉시 모듈 문법에 따라 전자책 파일을 생성하도록 로직 수정.
    - IOUtil.java: 불필요한 메서드 제거 후 임시 파일, 파일 라인 읽기 관련 메서드 추가.

--- makeabook-domain-rds/src/main/java/com/shacomiro/makeabook/domain/rds/ebookfile/service/EbookFileService.java
+++ makeabook-domain-rds/src/main/java/com/shacomiro/makeabook/domain/rds/ebookfile/service/EbookFileService.java
@@ -3,21 +3,26 @@ package com.shacomiro.makeabook.domain.rds.ebookfile.service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 import java.util.UUID;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.shacomiro.makeabook.domain.rds.ebookfile.entity.EbookFile;
 import com.shacomiro.makeabook.domain.rds.ebookfile.entity.EbookFileExtension;
 import com.shacomiro.makeabook.domain.rds.ebookfile.repository.EbookFileRepository;
 import com.shacomiro.makeabook.ebook.EbookManager;
+import com.shacomiro.makeabook.ebook.domain.ContentTempFileInfo;
 import com.shacomiro.makeabook.ebook.domain.EpubFileInfo;
 import com.shacomiro.makeabook.ebook.error.FileIOException;
+import com.shacomiro.makeabook.ebook.util.IOUtil;
 
 @Service
 @Transactional
@@ -33,15 +38,16 @@ public class EbookFileService {
 		this.ebookManager = new EbookManager(RESOURCES_DIR);
 	}
 
-	public EbookFile createEpub(ByteArrayResource resource, EbookFileExtension ebookFileExtension,
-			String fileName) {
+	public EbookFile createEpub(MultipartFile file, EbookFileExtension ebookFileExtension) {
 		String uuid = UUID.randomUUID().toString();
 		EpubFileInfo epubFileInfo;
 		EbookFile ebookFile = null;
 		String ebookExtension;
 
+		ContentTempFileInfo contentTempFileInfo = saveUploadToTempFile(file);
+
 		if (ebookFileExtension.equals(EbookFileExtension.EPUB2)) {
-			epubFileInfo = ebookManager.translateTxtToEpub2(resource.getByteArray(), uuid, fileName);
+			epubFileInfo = ebookManager.translateTxtToEpub2(uuid, file.getOriginalFilename(), contentTempFileInfo);
 			ebookExtension = EbookFileExtension.EPUB2.getEbookExt().toLowerCase();
 
 			ebookFile = ebookFileRepository
@@ -52,6 +58,12 @@ public class EbookFileService {
 							.fileExtension("epub")
 							.downloadUrl(downloadUrlPrefix + "/download/" + uuid)
 							.build());
+		}
+
+		try {
+			Files.deleteIfExists(contentTempFileInfo.getTxtTempFilePath());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 
 		return ebookFile;
@@ -77,5 +89,21 @@ public class EbookFileService {
 		} else {
 			return Optional.empty();
 		}
+	}
+
+	private ContentTempFileInfo saveUploadToTempFile(MultipartFile txtFile) {
+		Path tempUploadFilePath = IOUtil.createTempFile(
+				Paths.get(ebookManager.getContentsBasePath()).normalize().toAbsolutePath(),
+				"." + FilenameUtils.getExtension(txtFile.getOriginalFilename()));
+
+		try {
+			txtFile.transferTo(tempUploadFilePath);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return ContentTempFileInfo.builder()
+				.txtTempFilePath(tempUploadFilePath)
+				.build();
 	}
 }
--- makeabook-ebook/src/main/java/com/shacomiro/makeabook/ebook/util/IOUtil.java
+++ makeabook-ebook/src/main/java/com/shacomiro/makeabook/ebook/util/IOUtil.java
@@ -1,84 +1,37 @@
 package com.shacomiro.makeabook.ebook.util;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
-
-import com.shacomiro.makeabook.ebook.domain.Section;
 import com.shacomiro.makeabook.ebook.error.FileIOException;
-import com.shacomiro.makeabook.ebook.grammar.EbookGrammar;
 
 public class IOUtil {
 	private IOUtil() {
 		throw new IllegalStateException("Utility class");
 	}
 
-	public static void readStream(InputStream is, String encoding, List<Section> sectionList,
-			Map<String, String> metainfo) {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(is, encoding))) {
-			Section section = new Section();
-			List<String> paragraphList = new ArrayList<>();
-			boolean isNonSectionEbook = true;
-
-			while (br.ready()) {
-				String str = br.readLine();
-				boolean isParagraph = true;
-
-				if (StringUtils.isEmpty(str)) {
-					continue;
-				}
-
-				if (StringUtils.contains(str, EbookGrammar.BOOK_TITLE)) {
-					isParagraph = false;
-					metainfo.put("Title", StringUtils.split(str, EbookGrammar.BOOK_TITLE)[0]);
-				}
-
-				if (StringUtils.contains(str, EbookGrammar.BOOK_AUTHOR)) {
-					isParagraph = false;
-					metainfo.put("Author", StringUtils.split(str, EbookGrammar.BOOK_AUTHOR)[0]);
-				}
-
-				if (StringUtils.contains(str, EbookGrammar.SECTION_TITLE)) {
-					isParagraph = false;
-
-					if (isNonSectionEbook) {
-						isNonSectionEbook = false;
-					} else {
-						updateSectionList(sectionList, section, paragraphList);
-					}
-
-					paragraphList = new ArrayList<>();
-					section = Section.builder()
-							.title(StringUtils.split(str, EbookGrammar.SECTION_TITLE)[0])
-							.build();
-				}
-
-				if (isParagraph) {
-					paragraphList.add(str);
-				}
-			}
-
-			updateSectionList(sectionList, section, paragraphList);
+	public static List<String> readStringFromFile(Path path, Charset charset) {
+		try {
+			return Files.readAllLines(path, charset);
 		} catch (IOException e) {
-			throw new FileIOException("Fail to read stream of text file", e);
+			throw new FileIOException("Fail to read string from file", e);
 		}
 	}
 
-	private static void updateSectionList(List<Section> sectionList, Section section, List<String> paragraphList) {
-		section.updateParagraphList(paragraphList);
-		sectionList.add(section);
+	public static Path createTempFile(Path path, String extension) {
+		try {
+			return Files.createTempFile(path, "temp_", extension);
+		} catch (IOException e) {
+			throw new FileIOException("Fail to create temporary file", e);
+		}
 	}
 
 	public static InputStream loadFileToInputStream(Path path) {
