bbff759a22995b8d5d73ef3a56bc1b4cc7bc6777
Sunghyun Lee <98dltjdgus@naver.com>
2022-11-23 21:53:24 +0900

    refactor: API 성공 응답 제네릭 메서드 구체화
    - ApiUtils.java: 정적 클래스에서 응답할 데이터인 RepresentationModel<?> 클래스를 제네릭 타입 클래스 D로 구체화.

--- makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/util/ApiUtils.java
+++ makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/util/ApiUtils.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +20,13 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiUtils {
 
-	public static <T> ResponseEntity<?> success(T response,
-			RepresentationModelAssemblerSupport<T, ?> representationModelAssembler, HttpStatus status) {
+	public static <T, D extends RepresentationModel<D>> ResponseEntity<D> success(T response,
+			RepresentationModelAssemblerSupport<T, D> representationModelAssembler, HttpStatus status) {
 		return new ResponseEntity<>(representationModelAssembler.toModel(response).add(docsLink()), status);
 	}
 
-	public static <T> ResponseEntity<CollectionModel<?>> success(List<T> response,
-			RepresentationModelAssemblerSupport<T, ?> representationModelAssembler, HttpStatus status) {
+	public static <T, D extends RepresentationModel<D>> ResponseEntity<CollectionModel<D>> success(List<T> response,
+			RepresentationModelAssemblerSupport<T, D> representationModelAssembler, HttpStatus status) {
 		return new ResponseEntity<>(representationModelAssembler.toCollectionModel(response).add(docsLink()), status);
 	}
 
