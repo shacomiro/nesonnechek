ada3da7c38cab2ea7a2035e030c11355e04a54b7
Sunghyun Lee <98dltjdgus@naver.com>
2022-11-24 15:54:28 +0900

    style: 유저 엔티티의 변수 간 공백 제거
    - User.java: 유저 엔티티의 변수 간 공백 제거.

--- makeabook-domain-rds/src/main/java/com/shacomiro/makeabook/domain/rds/user/entity/User.java
+++ makeabook-domain-rds/src/main/java/com/shacomiro/makeabook/domain/rds/user/entity/User.java
@@ -22,22 +22,16 @@ public class User {
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
 	@Column(name = "email")
 	private String email;
-
 	@Column(name = "password")
 	private String password;
-
 	@Column(name = "username")
 	private String username;
-
 	@Column(name = "login_count")
 	private int loginCount;
-
 	@Column(name = "last_login_at")
 	private LocalDateTime lastLoginAt;
-
 	@Column(name = "created_at")
 	private LocalDateTime createdAt;
 }
