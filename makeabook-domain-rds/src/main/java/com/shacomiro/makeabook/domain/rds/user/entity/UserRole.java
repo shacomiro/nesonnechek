646c9bfb94d86dd5730bd6ff9108e6c2842e6236
Sunghyun Lee <98dltjdgus@naver.com>
2022-12-15 20:45:03 +0900

    feat: 유저 역할 열거형 클래스 추가
    - UserRole.java: 유저가 가질 수 있는 역할들을 갖는 열거형 클래스 추가.

---
+++ makeabook-domain-rds/src/main/java/com/shacomiro/makeabook/domain/rds/user/entity/UserRole.java
@@ -0,0 +1,10 @@
+package com.shacomiro.makeabook.domain.rds.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UserRole {
+	USER();
+}
