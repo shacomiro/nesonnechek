e2a83706fff1b24cb8907f7df8c3dd2257750e84
Sunghyun Lee <98dltjdgus@naver.com>
2022-12-20 00:14:46 +0900

    feat: 패스워드 인코더 구현
    - WebSecurityConfiguration.java: BCrypt 기반 패스워드 인코더 를 타 클래스에서 주입 받아 사용할 수 있도록 Bean으로 등록.

--- makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/config/WebSecurityConfiguration.java
+++ makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/config/WebSecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,6 +26,11 @@ public class WebSecurityConfiguration {
 	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 	private final ObjectMapper objectMapper;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
