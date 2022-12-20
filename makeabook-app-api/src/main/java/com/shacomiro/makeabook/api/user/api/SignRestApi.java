a950399c888140f3cf8ee5c5b9cf37956489b4bf
Sunghyun Lee <98dltjdgus@naver.com>
2022-12-20 19:31:32 +0900

    feat: 회원 가입 결과 응답을 유저 모델로 변경
    - SignRestApi.java: 회원 가입된 유저 엔티티 정보 중 일부를 유저 모델로 변환한 후, 로그인(signin) 링크와 API 문서 링크를 함께 담아 응답하도록 응답 결과 수정.

--- makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/user/api/SignRestApi.java
+++ makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/user/api/SignRestApi.java
@@ -1,5 +1,8 @@
 package com.shacomiro.makeabook.api.user.api;
 
+import static com.shacomiro.makeabook.api.global.util.ApiUtils.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -11,8 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shacomiro.makeabook.api.user.dto.SignUpRequest;
+import com.shacomiro.makeabook.api.user.dto.model.UserModel;
 import com.shacomiro.makeabook.domain.rds.user.dto.SignUpDto;
 import com.shacomiro.makeabook.domain.rds.user.entity.Email;
+import com.shacomiro.makeabook.domain.rds.user.entity.User;
 import com.shacomiro.makeabook.domain.rds.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +28,11 @@ import lombok.RequiredArgsConstructor;
 public class SignRestApi {
 	private final UserService userService;
 	private final PasswordEncoder passwordEncoder;
+
+	@PostMapping(path = "signin")
+	public ResponseEntity<?> signIn() {
+		return null;
+	}
 
 	@PostMapping(path = "signup")
 	public ResponseEntity<?> singUp(@RequestBody @Valid SignUpRequest signUpRequest) {
@@ -38,6 +48,15 @@ public class SignRestApi {
 				signUpRequest.getUsername()
 		);
 
-		return new ResponseEntity<>(userService.save(signUpDto), HttpStatus.CREATED);
+		User signUpUser = userService.save(signUpDto);
+
+		UserModel userModel = UserModel.builder()
+				.email(signUpUser.getEmail().getValue())
+				.createdAt(signUpUser.getCreatedAt())
+				.build();
+		userModel.add(linkTo(methodOn(SignRestApi.class).signIn()).withRel("signin"));
+		userModel.add(docsLink());
+
+		return new ResponseEntity<>(userModel, HttpStatus.CREATED);
 	}
 }
