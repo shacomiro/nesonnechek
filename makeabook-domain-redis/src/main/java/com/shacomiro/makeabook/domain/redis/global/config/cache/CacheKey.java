ee2bb8dab6cca2e82332fabc5ef815d9d1ed097f
Sunghyun Lee <98dltjdgus@naver.com>
2022-12-28 17:19:05 +0900

    refactor: 캐시 유효 시간 단위 변경
    - CacheKey.java: 캐시의 유효 시간 단위를 ms로 변경.

--- makeabook-domain-redis/src/main/java/com/shacomiro/makeabook/domain/redis/global/config/cache/CacheKey.java
+++ makeabook-domain-redis/src/main/java/com/shacomiro/makeabook/domain/redis/global/config/cache/CacheKey.java
@@ -1,8 +1,11 @@
 package com.shacomiro.makeabook.domain.redis.global.config.cache;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CacheKey {
-	public static final long DEFAULT_EXPIRE_SEC = 60L;
+	public static final long DEFAULT_EXPIRE_MILLIS_SEC = 1000L * 60 * 3;
 }
