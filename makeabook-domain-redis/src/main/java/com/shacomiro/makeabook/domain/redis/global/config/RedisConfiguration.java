48666c5e75878e51e7734f956b873ce70e846931
Sunghyun Lee <98dltjdgus@naver.com>
2022-12-28 19:09:27 +0900

    feat: Redis 이벤트 수신 설정 추가
    - RedisConfiguration.java: Redis keyspace notification으로부터 expire 이벤트를 수신하도록 설정하여 TTL이 지정된 데이터의 정합성을 유지하도록 수정.

--- makeabook-domain-redis/src/main/java/com/shacomiro/makeabook/domain/redis/global/config/RedisConfiguration.java
+++ makeabook-domain-redis/src/main/java/com/shacomiro/makeabook/domain/redis/global/config/RedisConfiguration.java
@@ -1,9 +1,12 @@
 package com.shacomiro.makeabook.domain.redis.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
-@EnableRedisRepositories({"com.shacomiro.makeabook.domain.redis"})
+// @EntityScan({"com.shacomiro.makeabook.domain.redis"}) //필요 여부 확인 요망
+@EnableRedisRepositories(value = {"com.shacomiro.makeabook.domain.redis"},
+		enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfiguration {
 }
