8faea7481aaef6b3b697c3f192503058e800be3f
Sunghyun Lee <98dltjdgus@naver.com>
2022-12-29 22:28:16 +0900

    feat: Apiresult 객체 복구
    - ApiUtils.java: 에러 정보를 포함하는 ApiResult 객체 재구현.

--- makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/util/ApiUtils.java
+++ makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/util/ApiUtils.java
@@ -22,7 +22,11 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiUtils {
 
-	public static <T, D extends RepresentationModel<D>> ResponseEntity<D> success(
+	public static <T> ResponseEntity<EntityModel<ApiResult<T>>> success(T response, HttpStatus status) {
+		return new ResponseEntity<>(result(true, response, null), status);
+	}
+
+	public static <T, D extends RepresentationModel<D>> ResponseEntity<EntityModel<ApiResult<D>>> success(
 			T response,
 			RepresentationModelAssemblerSupport<T, D> representationModelAssembler,
 			HttpStatus status) {
@@ -45,12 +49,16 @@ public class ApiUtils {
 				status);
 	}
 
-	public static EntityModel<ApiError> error(Throwable throwable, HttpStatus status) {
-		return EntityModel.of(new ApiError(throwable, status), docsLink());
+	public static <T> EntityModel<ApiResult<T>> result(boolean success, T response, ApiError error) {
+		return EntityModel.of(new ApiResult<>(success, response, error), docsLink());
 	}
 
-	public static EntityModel<ApiError> error(String message, HttpStatus status) {
-		return EntityModel.of(new ApiError(message, status), docsLink());
+	public static <T> EntityModel<ApiResult<T>> error(Throwable throwable, HttpStatus status) {
+		return EntityModel.of(new ApiResult<>(false, null, new ApiError(throwable, status)), docsLink());
+	}
+
+	public static <T> EntityModel<ApiResult<T>> error(String message, HttpStatus status) {
+		return EntityModel.of(new ApiResult<>(false, null, new ApiError(message, status)), docsLink());
 	}
 
 	public static String getCurrentApiRequest() {
@@ -82,6 +90,20 @@ public class ApiUtils {
 		ApiError(String message, HttpStatus status) {
 			this.message = message;
 			this.status = status.value();
+		}
+	}
+
+	@ToString
+	@Getter
+	public static class ApiResult<T> {
+		private final boolean success;
+		private final T response;
+		private final ApiError error;
+
+		ApiResult(boolean success, T response, ApiError error) {
+			this.success = success;
+			this.response = response;
+			this.error = error;
 		}
 	}
 }
