e345053b93569ac290439f5156bf82dc2c3270fb
Sunghyun Lee <98dltjdgus@naver.com>
2023-01-02 18:22:40 +0900

    fix: 누락된 import 구문 추가
    JacksonConfiguration.java: 누락된 SerializationFeature improt 구문 추가.

--- makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/config/JacksonConfiguration.java
+++ makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/config/JacksonConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.shacomiro.makeabook.api.global.security.deserializer.SimpleGrantedAuthorityDeserializer;
 
