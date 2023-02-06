439a40dc430775d043b765447c26adcfb58b0df3
Sunghyun Lee <98dltjdgus@naver.com>
2023-02-06 20:08:54 +0900

    test: 전자책 모듈 테스트의 디렉터리 설정 수정
    - EbookManagerTest.java: 변경된 전자책 모듈 디렉터리 설정 반영.

--- java-epub-generator/src/test/java/com/shacomiro/makeabook/ebook/EbookManagerTest.java
+++ java-epub-generator/src/test/java/com/shacomiro/makeabook/ebook/EbookManagerTest.java
@@ -1,11 +1,14 @@
 package com.shacomiro.makeabook.ebook;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -20,7 +23,15 @@ import com.shacomiro.epub.domain.EpubFileInfo;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class EbookManagerTest {
-	static final String testFilePath = "./src/test/resources";
+	static final String testResourceDir = "./src/test/resources";
+	static final String testContentDir = "./src/test/resources/content";
+	static final String testEbookDir = "./src/test/resources/ebook";
+
+	@BeforeAll
+	public void initDirectories() throws IOException {
+		Files.createDirectories(Paths.get(testContentDir));
+		Files.createDirectories(Paths.get(testEbookDir));
+	}
 
 	@Test
 	@Order(1)
@@ -29,16 +40,15 @@ class EbookManagerTest {
 		//given
 		String uuid = UUID.randomUUID().toString();
 		String fileName = "애국가.txt";
-		Path expectTestResultPath = Paths.get(testFilePath,
-						File.separatorChar + "ebook" + File.separatorChar + "epub2" + File.separatorChar + uuid + ".epub")
+		Path expectTestResultPath = Paths.get(testEbookDir, File.separatorChar + uuid + ".epub")
 				.toAbsolutePath()
 				.normalize();
 		ContentTempFileInfo contentTempFileInfo = new ContentTempFileInfo(uuid,
-				Paths.get(testFilePath, File.separatorChar + fileName).normalize().toAbsolutePath(), fileName);
+				Paths.get(testResourceDir, File.separatorChar + fileName).normalize().toAbsolutePath(), fileName);
 
 		//when
-		EpubManager ebookManager = new EpubManager(testFilePath);
-		EpubFileInfo info = ebookManager.translateTxtToEpub2(uuid, fileName, contentTempFileInfo);
+		EpubManager ebookManager = new EpubManager(testContentDir, testEbookDir);
+		EpubFileInfo info = ebookManager.translateTxtToEpub2(uuid, contentTempFileInfo);
 
 		//then
 		Assertions.assertEquals(expectTestResultPath, info.getFilePath());
@@ -53,14 +63,14 @@ class EbookManagerTest {
 		String uuid = UUID.randomUUID().toString();
 		String fileName = "빈파일.txt";
 		ContentTempFileInfo contentTempFileInfo = new ContentTempFileInfo(uuid,
-				Paths.get(testFilePath, File.separatorChar + fileName).normalize().toAbsolutePath(), fileName);
+				Paths.get(testResourceDir, File.separatorChar + fileName).normalize().toAbsolutePath(), fileName);
 
 		//when
-		EpubManager ebookManager = new EpubManager(testFilePath);
+		EpubManager ebookManager = new EpubManager(testContentDir, testEbookDir);
 
 		//then
 		Assertions.assertThrows(NullPointerException.class,
-				() -> ebookManager.translateTxtToEpub2(uuid, fileName, contentTempFileInfo));
+				() -> ebookManager.translateTxtToEpub2(uuid, contentTempFileInfo));
 	}
 
 }
