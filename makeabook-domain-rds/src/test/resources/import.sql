6cfb7efb43c4ab7ab6cb9271bd5c6277aab287cc
Sunghyun Lee <98dltjdgus@naver.com>
2023-02-14 22:59:48 +0900

    chore: H2 인메모리 DB 기반 테스트를 위한 환경 설정 추가
    - makeabook-domain-rds\build.gradle: RDS 모듈에 H2 데이터베이스 라이브러리 의존성 추가.
    - application-domain-rds-test.yaml: H2 인메모리 DB 전용 테스트 구성 파일 설정.
    - import.sql: 예제 DB 데이터 삽입문 추가.

---
+++ makeabook-domain-rds/src/test/resources/import.sql
@@ -0,0 +1,19 @@
+INSERT INTO user_tb (email, password, username, login_count, last_login_at, created_at) VALUES ('user1@email.com', 'user1_password', 'USER1', 3, TIMESTAMP '2022-12-30 12:00:00', TIMESTAMP '2022-08-20 12:00:00');
+INSERT INTO user_tb (email, password, username, login_count, last_login_at, created_at) VALUES ('user2@email.com', 'user2_password', 'USER2', 1, TIMESTAMP '2023-01-16 13:30:00', TIMESTAMP '2022-09-21 13:30:00');
+INSERT INTO user_tb (email, password, username, login_count, last_login_at, created_at) VALUES ('user3@email.com', 'user3_password', 'USER3', 5, TIMESTAMP '2022-01-11 09:00:00', TIMESTAMP '2022-10-11 09:00:00');
+INSERT INTO user_tb (email, password, username, login_count, last_login_at, created_at) VALUES ('user4@email.com', 'user4_password', 'USER4', 3, TIMESTAMP '2022-12-06 19:20:00', TIMESTAMP '2022-12-03 19:20:00');
+INSERT INTO user_tb (email, password, username, login_count, last_login_at, created_at) VALUES ('user5@email.com', 'user5_password', 'USER5', 6, null, TIMESTAMP '2022-12-29 14:50:00');
+
+INSERT INTO user_roles_tb (user_id, role) VALUES (1, 'USER');
+INSERT INTO user_roles_tb (user_id, role) VALUES (2, 'USER');
+INSERT INTO user_roles_tb (user_id, role) VALUES (3, 'USER');
+INSERT INTO user_roles_tb (user_id, role) VALUES (4, 'USER');
+INSERT INTO user_roles_tb (user_id, role) VALUES (5, 'USER');
+
+INSERT INTO ebook_tb (user_id, uuid, name, type, download_count, created_at, expired_at, is_exist) VALUES (1, '550e8400-e29b-41d4-a716-446655440001', 'ebook-1', 'EPUB2', 3, TIMESTAMP '2022-11-29 12:00:00', TIMESTAMP '2022-12-29 12:00:00', false);
+INSERT INTO ebook_tb (user_id, uuid, name, type, download_count, created_at, expired_at, is_exist) VALUES (2, '550e8400-e29b-41d4-a716-446655440002', 'ebook-2', 'EPUB2', 2, TIMESTAMP '2022-11-30 12:30:00', TIMESTAMP '2023-12-30 12:30:00', false);
+INSERT INTO ebook_tb (user_id, uuid, name, type, download_count, created_at, expired_at, is_exist) VALUES (4, '550e8400-e29b-41d4-a716-446655440003', 'ebook-3', 'EPUB2', 1, TIMESTAMP '2022-12-05 17:00:00', TIMESTAMP '2023-01-04 17:00:00', false);
+INSERT INTO ebook_tb (user_id, uuid, name, type, download_count, created_at, expired_at, is_exist) VALUES (2, '550e8400-e29b-41d4-a716-446655440004', 'ebook-4', 'EPUB2', 5, TIMESTAMP '2022-12-08 08:20:00', TIMESTAMP '2023-01-07 08:20:00', false);
+INSERT INTO ebook_tb (user_id, uuid, name, type, download_count, created_at, expired_at, is_exist) VALUES (3, '550e8400-e29b-41d4-a716-446655440005', 'ebook-5', 'EPUB2', 0, TIMESTAMP '2023-01-10 20:10:00', TIMESTAMP '2023-02-09 20:10:00', true);
+INSERT INTO ebook_tb (user_id, uuid, name, type, download_count, created_at, expired_at, is_exist) VALUES (5, '550e8400-e29b-41d4-a716-446655440006', 'ebook-6', 'EPUB2', 1, TIMESTAMP '2023-01-12 16:40:00', TIMESTAMP '2023-02-11 16:40:00', true);
+INSERT INTO ebook_tb (user_id, uuid, name, type, download_count, created_at, expired_at, is_exist) VALUES (2, '550e8400-e29b-41d4-a716-446655440007', 'ebook-7', 'EPUB2', 2, TIMESTAMP '2023-01-14 13:20:00', TIMESTAMP '2023-02-13 13:20:00', true);
