70c2bf750abf01eef6ef0d3993be688c3f7fc407
Sunghyun Lee <98dltjdgus@naver.com>
2023-02-21 19:40:25 +0900

    fix: 잘못 지정된 YAML 구성 프로퍼티 수정
    - JwtConfiguration.java: 외부 구성 프로퍼티에 직접 접근하는 대신 내부 구성 파일에 적용된 프로퍼티를 사용하도록 수정.
    - makeabook-app-api\src\main\resources\application.yaml: JWT 모듈을 사용하는 API 모듈의 구성 파일에 JWT 관련 프로퍼티 값 정의.
    - makeabook-domain\src\main\resources\application-domain.yaml: 도메인에서 직접 사용하지 않는 JWT 프로퍼티 값 제거.

--- makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/config/token/JwtConfiguration.java
+++ makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/config/token/JwtConfiguration.java
@@ -14,16 +14,16 @@ public class JwtConfiguration {
 	private final long accessTokenValidMilliseconds;
 	private final long refreshTokenValidMilliseconds;
 
-	public JwtConfiguration(@Value("${ext-config.jwt.issuer}") String jwtIssuer,
-			@Value("${ext-config.jwt.valid-milliseconds.access}") long accessTokenValidMilliseconds,
-			@Value("${ext-config.jwt.valid-milliseconds.refresh}") long refreshTokenValidMilliseconds) {
+	public JwtConfiguration(@Value("${jwt.issuer}") String jwtIssuer,
+			@Value("${jwt.valid-milliseconds.access}") long accessTokenValidMilliseconds,
+			@Value("${jwt.valid-milliseconds.refresh}") long refreshTokenValidMilliseconds) {
 		this.jwtIssuer = jwtIssuer;
 		this.accessTokenValidMilliseconds = accessTokenValidMilliseconds;
 		this.refreshTokenValidMilliseconds = refreshTokenValidMilliseconds;
 	}
 
 	@Bean
-	public JwtProvider jwtProvider(@Value("${ext-config.jwt.secret.key}") String secretKey) {
+	public JwtProvider jwtProvider(@Value("${jwt.secret.key}") String secretKey) {
 		return new JwtProvider(secretKey, SignatureAlgorithm.HS256);
 	}
 
