975fe6abd9a844e7ef1ba46a79aec22b0b7f55f6
Sunghyun Lee <98dltjdgus@naver.com>
2023-02-23 21:29:38 +0900

    refactor: 주입받는 Value값 프로퍼티명 변경
    - AwsS3Configuration.java: 변경된 YAML 구성 프로퍼티명을 사용하도록 변수명 변경.

--- makeabook-domain/src/main/java/com/shacomiro/makeabook/domain/global/config/aws/AwsS3Configuration.java
+++ makeabook-domain/src/main/java/com/shacomiro/makeabook/domain/global/config/aws/AwsS3Configuration.java
@@ -10,15 +10,15 @@ import com.shacomiro.aws.s3.AwsS3ClientManager;
 public class AwsS3Configuration {
 	private final String bucketName;
 
-	public AwsS3Configuration(@Value("${ext-config.aws.s3.bucket}") String bucketName) {
+	public AwsS3Configuration(@Value("${aws.s3.bucket}") String bucketName) {
 		this.bucketName = bucketName;
 	}
 
 	@Bean
 	public AwsS3ClientManager awsS3ClientManager(
-			@Value("${ext-config.aws.s3.access-key}") String accessKey,
-			@Value("${ext-config.aws.s3.secret-key}") String secretKey,
-			@Value("${ext-config.aws.s3.region}") String region
+			@Value("${aws.s3.key.access}") String accessKey,
+			@Value("${aws.s3.key.secret}") String secretKey,
+			@Value("${aws.s3.region}") String region
 	) {
 		return new AwsS3ClientManager(accessKey, secretKey, region);
 	}
