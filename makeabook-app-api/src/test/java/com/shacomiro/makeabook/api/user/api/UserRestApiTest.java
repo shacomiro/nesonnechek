5c04ed325748a47beb3e0c5bac26a1ce3569cd01
Sunghyun Lee <98dltjdgus@naver.com>
2023-03-16 21:35:29 +0900

    test: MockMvc 구성 방식 변경
    - UserRestApiTest.java: MockMvc 구성을 테스트 클래스의 setUp 메서드 내에서 수행하도록 변경. ASCII Doc 적용을 위해 RestDocumentationContextProvider 문서화 설정 적용.

--- makeabook-app-api/src/test/java/com/shacomiro/makeabook/api/user/api/UserRestApiTest.java
+++ makeabook-app-api/src/test/java/com/shacomiro/makeabook/api/user/api/UserRestApiTest.java
@@ -1,30 +1,47 @@
 package com.shacomiro.makeabook.api.user.api;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import com.shacomiro.makeabook.api.global.config.mock.annotation.EnableMockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shacomiro.makeabook.api.global.security.dto.JwtDto;
+import com.shacomiro.makeabook.api.global.security.filter.JwtAuthenticationFilter;
+import com.shacomiro.makeabook.api.global.security.filter.JwtReissueFilter;
 import com.shacomiro.makeabook.api.global.security.service.JwtProvisionService;
 
 @SpringBootTest
-@EnableMockMvc
+@ExtendWith(value = {RestDocumentationExtension.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource(properties = {"spring.config.location = classpath:application-test.yaml"})
 class UserRestApiTest {
 	private static final String USER_EMAIL = "user1@email.com";
-	@Autowired
 	private MockMvc mockMvc;
 	private JwtDto jwtDto;
 
 	@BeforeEach
-	void setUp(@Autowired JwtProvisionService jwtProvisionService) {
+	void setUp(
+			RestDocumentationContextProvider restDocumentation, @Autowired WebApplicationContext context,
+			@Autowired AuthenticationManager authenticationManager, @Autowired ObjectMapper objectMapper,
+			@Autowired JwtProvisionService jwtProvisionService
+	) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+				.apply(documentationConfiguration(restDocumentation))
+				.addFilter(new JwtAuthenticationFilter(authenticationManager, objectMapper))
+				.addFilter(new JwtReissueFilter(jwtProvisionService, objectMapper))
+				.build();
 		jwtDto = jwtProvisionService.issueJwt(USER_EMAIL);
 	}
 
