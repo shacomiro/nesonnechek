a5bd99e703cbb0edf5ca3698ee91c63111cbee4f
Sunghyun Lee <98dltjdgus@naver.com>
2023-03-20 22:32:32 +0900

    test: 기초 테스트 코드 구현
    - UserRestApiTest.java: mockMvc를 이용한 기본적인 API 테스트 로직 작동 확인.

--- makeabook-app-api/src/test/java/com/shacomiro/makeabook/api/user/api/UserRestApiTest.java
+++ makeabook-app-api/src/test/java/com/shacomiro/makeabook/api/user/api/UserRestApiTest.java
@@ -1,18 +1,27 @@
 package com.shacomiro.makeabook.api.user.api;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -39,6 +48,9 @@ class UserRestApiTest {
 	) {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
 				.apply(documentationConfiguration(restDocumentation))
+				.alwaysDo(document("{class-name}/{method-name}",
+						preprocessRequest(prettyPrint()),
+						preprocessResponse(prettyPrint())))
 				.addFilter(new JwtAuthenticationFilter(authenticationManager, objectMapper))
 				.addFilter(new JwtReissueFilter(jwtProvisionService, objectMapper))
 				.build();
@@ -48,5 +60,23 @@ class UserRestApiTest {
 	@AfterEach
 	void tearDown() {
 		jwtDto = null;
+	}
+
+	@Test
+	@Order(1)
+	@DisplayName("유저 정보 조회")
+	void getAccountInfo() throws Exception {
+		//given
+		String url = "/api/v1/users/account";
+		String bearerToken = "Bearer " + jwtDto.getAccessToken();
+
+		//when
+		ResultActions result = mockMvc.perform(
+				get(url).header(HttpHeaders.AUTHORIZATION, bearerToken)
+		);
+
+		//then
+		result.andExpect(status().isOk())
+				.andDo(print());
 	}
 }
