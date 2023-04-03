533eb7fa3d1454fbf8ffca70c5771bf8738fee51
Sunghyun Lee <98dltjdgus@naver.com>
2023-04-03 13:58:32 +0900

    feat: JPA 엔티티의 @OneToMany, @ManyToOne 관계 명시적 선언
    - Ebook.java: 전자책 엔티티의 유저 정보 즉시 로딩을 명시적으로 선언.
    - User.java: 유저 엔티티의 전자책 목록 정보 지연 로딩을 명시적으로 선언.

--- makeabook-domain-rds/src/main/java/com/shacomiro/makeabook/domain/rds/user/entity/User.java
+++ makeabook-domain-rds/src/main/java/com/shacomiro/makeabook/domain/rds/user/entity/User.java
@@ -3,6 +3,7 @@ package com.shacomiro.makeabook.domain.rds.user.entity;
 import static java.time.LocalDateTime.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.AttributeOverride;
@@ -18,12 +19,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.BatchSize;
+
+import com.shacomiro.makeabook.domain.rds.ebook.entity.Ebook;
 import com.shacomiro.makeabook.domain.rds.global.validation.annotation.ValidEnumCollection;
 
 import lombok.AccessLevel;
@@ -65,6 +70,9 @@ public class User {
 	@Column(name = "role")
 	@ValidEnumCollection(enumClass = UserRole.class, message = "User role value is invalid")
 	private List<UserRole> roles;
+	@BatchSize(size = 100)
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	private List<Ebook> ebooks;
 
 	@Builder(builderClassName = "BySignUpInfos", builderMethodName = "bySignUpInfos")
 	public User(Email email, String encryptedPassword, String username, List<UserRole> roles) {
@@ -90,6 +98,7 @@ public class User {
 		this.lastLoginAt = lastLoginAt;
 		this.createdAt = createdAt;
 		this.roles = roles;
+		this.ebooks = new ArrayList<>();
 	}
 
 	public void afterLoginSuccess() {
