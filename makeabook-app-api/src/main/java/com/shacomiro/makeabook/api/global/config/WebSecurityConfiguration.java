0051efd7d926ae7ed2dc67c2122ba2ce50952c7a
Sunghyun Lee <98dltjdgus@naver.com>
2023-04-15 21:07:37 +0900

    refactor: API 문서 접근 URL 수정
    - WebSecurityConfiguration.java: 잘못 지정된 API 문서 파일 접근 URL을 올바르게 수정.

--- makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/config/WebSecurityConfiguration.java
+++ makeabook-app-api/src/main/java/com/shacomiro/makeabook/api/global/config/WebSecurityConfiguration.java
@@ -61,7 +61,7 @@ public class WebSecurityConfiguration {
 				.authorizeRequests()
 				.antMatchers("/api/v1/auth/sign-in").permitAll()
 				.antMatchers("/api/v1/auth/sign-up").permitAll()
-				.antMatchers("/docs/**").permitAll()
+				.antMatchers("/api/docs/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.exceptionHandling()
